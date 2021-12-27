2fa7b6c43b3fa6c8c94f6257aa786b87aef1c3ca
Asadbek Rahimov <asadbek.3422@gmail.com>
2021-12-27 18:05:18 +0500

    bot

---
+++ Kirill-Lotin bot/Kirill-Lotin bot.py
@@ -0,0 +1,33 @@
+# 27/12/2021
+# Loyiha muallifi: Rahimov Asadbek
+# Ustoz: Anvar Narzullayev
+# Manba: mohirdev.uz
+# 27-dars. Kirill-Lotin bot
+
+from transliterate import to_latin, to_cyrillic
+import telebot
+
+TOKEN = '5081864413:AAG9UxqbKk24aCrbRMExoIdd2bAHtcK4ILo'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    javob = """Assalomu alaykum, Xush kelibsiz! Matn kiriting: 
+Здравствуйте, добро пожаловать! Введите текст:
+Hello, Welcome! Enter the text:"""
+    bot.reply_to(message, javob)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    msg = message.text
+    javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    # if msg.isascii():
+    #     javob = to_cyrillic(msg)
+    # else:
+    #     javob = to_latin(msg)
+    bot.reply_to(message, javob(msg))
+
+
+bot.infinity_polling()
